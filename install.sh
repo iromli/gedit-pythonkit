dcce3e1970eeccee3281cc1ffcdc8c7aa2eec555
Isman Firmansyah <izman.romli@gmail.com>
2011-09-08 08:52:15 +0700

    remove language spec(s) as pythonkit doesnt care about it anymore

--- install.sh
+++ install.sh
@@ -2,16 +2,9 @@
 
 PYTHONKIT=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 GEDIT_PLUGIN_DIR=~/.gnome2/gedit/plugins
-LANGUAGE_SPEC_DIR=~/.local/share/gtksourceview-2.0/language-specs
 
 echo "installing pythonkit plugin"
 if [[ ! -d $GEDIT_PLUGIN_DIR ]]; then
     mkdir -p $GEDIT_PLUGIN_DIR
 fi
 cp -R $PYTHONKIT/plugin/pythonkit* $GEDIT_PLUGIN_DIR
-
-echo "installing dtl language-spec"
-if [[ ! -d $LANGUAGE_SPEC_DIR ]]; then
-    mkdir -p $LANGUAGE_SPEC_DIR
-fi
-cp $PYTHONKIT/language-spec/dtl.lang $LANGUAGE_SPEC_DIR
